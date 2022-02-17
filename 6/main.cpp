47ac43431c9266e8859571d23154e9a331759bfe
Graphdark <67462210+Graphdark@users.noreply.github.com>
2022-02-17 16:05:05 +0400

    Update main.cpp

--- 6/main.cpp
+++ 6/main.cpp
@@ -1,128 +1,233 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
 
-
-#define SIZE 15
-
-void exercise_1();
-void exercise_2_3();
-
-
-typedef enum {
-	BRACE_OPENLEFT,
-	BRACE_MISSINGLEFT,
-	BRACE_NONCONFORMITY,
-	BRACE_CONFORMITY
-} bMessage;
-
-bool checkLeftBrace(char);
-bool checkRightBrace(char);
-char getBraceLeftPair(char);
-bool checkBraces(char* string, int size);
-void printBraceMessage();
-
-
-typedef enum {
-	L_SORTASC,
-	L_SORTDESC,
-	L_NOTSORT,
-} lMessage;
-
-typedef struct Node
+struct Node
 {
-	int dat;
-	struct Node* next;
-} Node;
+	char data;
+	Node* next;
+};
 
-typedef struct
+struct List
 {
 	Node* head;
 	int size;
-} List;
+};
 
-List* init();
-void freeList(List*);
-void freeNode(Node*);
-
-void ins(List*, int);
-Node* rm(List*, int);
-
-List* copy(List*);
-void copy2(List*, List*);
-
-void printNode(Node*);
-void printList(List*);
-
-bool checkSort(List*);
-void printListMessage();
-
-
-
-int main(const int arc, const char** argv)
+void init(List* lst)
 {
-	exercise_1();
-	exercise_2_3();
-
-	return 0;
+	lst->head = NULL;
+	lst->size = 0;
 }
 
-void exercise_1()
+void ins(List* lst, char data)
 {
-	printf("Задание 1\n");
-	char str1[SIZE] = "(2+(2*2))";
-	printf("%s - ", checkBraces(str1, SIZE) ? "true" : "false");
-	printBraceMessage();
-	char str2[SIZE] = "{2/{5*(4+7)]";
-	printf("%s - ", checkBraces(str2, SIZE) ? "true" : "false");
-	printBraceMessage();
+	Node* newNode = new Node;
+	newNode->data = data;
+	newNode->next = NULL;
+
+	Node* current = new Node;
+	current = lst->head;
+	if (current == NULL)
+	{
+		lst->head = newNode;
+		lst->size++;
+	}
+	else
+	{
+		while (current->next != NULL)
+			current = current->next;
+		current->next = newNode;
+		lst->size++;
+	}
 }
 
-void exercise_2_3()
+void deleteList(List* lst)
 {
-	printf("\nЗадание 2 и 3\n");
-	List* lst1 = init();
-	ins(lst1, 123);
-	ins(lst1, 653);
-	ins(lst1, 947);
-	printf("Печать первого списка после добваления в него элементов:\n");
-	printList(lst1);
+	Node* current = new Node;
+	Node* nextNode = new Node;
+	current = lst->head;
+	while (current->next != NULL)
+	{
+		nextNode = current->next;
+		lst->head = nextNode;
+		lst->size--;
+		delete current;
+		current = nextNode;
+	}
+	delete nextNode;
+}
 
-	List* lst2 = copy(lst1);
-	printf("Печать второго списка после копирования:\n");
-	printList(lst2);
-	printf("%s - ", checkSort(lst2) ? "true" : "false");
-	printListMessage();
-	ins(lst2, 453);
-	printf("Печать второго списка после добваления в него элементов:\n");
-	printList(lst2);
-	printf("%s - ", checkSort(lst2) ? "true" : "false");
-	printListMessage();
+void printList(List* lst)
+{
+	Node* current = new Node;
+	current = lst->head;
+	if (current == NULL)
+	{
+		std::cout << "List is empty" << std::endl;
+	}
+	else
+	{
+		do
+		{
+			std::cout << int(current->data) << " ";
+			current = current->next;
+		} while (current != NULL);
 
-	free(rm(lst1, 123));
-	printf("Печать первого списка после удаления из него элементов:\n");
-	printList(lst1);
-	printf("Печать второго списка:\n");
-	printList(lst2);
+		std::cout << std::endl;
+	}
+}
 
-	free(rm(lst2, 947));
-	printf("Печать первого списка:\n");
-	printList(lst1);
-	printf("Печать второго списка после удаления из него элементов:\n");
-	printList(lst2);
+bool checkBracket(List* list)
+{
+	int flag1 = 0, flag2 = 0, flag3 = 0;
+	Node* current = new Node;
+	current = list->head;
+	if (current == NULL)
+	{
+		std::cout << "List is empty" << std::endl;
+		return false;
+	}
+	else
+	{
+		do
+		{
+			if (current->data == '(') flag1++;
+			if (current->data == ')') flag1--;
+			if (current->data == '{') flag2++;
+			if (current->data == '}') flag2--;
+			if (current->data == '[') flag3++;
+			if (current->data == ']') flag3--;
+			current = current->next;
+		} while (current != NULL);
 
-	free(rm(lst2, 123));
-	free(rm(lst2, 653));
-	ins(lst2, 452);
-	ins(lst2, 321);
-	printf("Печать второго списка после удаления/добавления в него элементов:\n");
-	printList(lst2);
-	printf("%s - ", checkSort(lst2) ? "true" : "false");
-	printListMessage();
+		if (flag1 == 0 && flag2 == 0 && flag3 == 0)
+		{
+			std::cout << "Bracket sequence is correct" << std::endl;
+			return true;
+		}
+		else
+		{
+			std::cout << "Bracket sequence is NOT correct" << std::endl;
+			return false;
+		}
+	}
+}
 
-	copy2(lst1, lst2);
-	printf("Печать второго списка после копирования в него элементов из первого списка:\n");
-	printList(lst2);
+List* copyList(List* lst)
+{
+	//old
+	Node* current = new Node;
+	current = lst->head;
 
-	freeList(lst1);
-	freeList(lst2);
+	if (current == NULL) {
+		std::cout << "List is empty!" << std::endl;
+		return NULL;
+	}
+
+	//new
+	List* newLst = new List;
+	init(newLst);
+
+	for (size_t i = 0; i < lst->size; i++)
+	{
+		ins(newLst, current->data);
+		current = current->next;
+	}
+	return newLst;
+}
+
+bool checkSort(List* lst)
+{
+	Node* current = new Node;
+	current = lst->head;
+	if (current == NULL)
+	{
+		std::cout << "List is empty" << std::endl;
+		return false;
+	}
+	else
+	{
+		int max = 0;
+		int min = 100;
+		int sortMax = 0;
+		int sortMin = 0;
+		do
+		{
+			if (current->data > max && sortMax == 0)
+				max = current->data;
+			else
+			{
+				sortMax = 1;
+				if (current->data < min && sortMin == 0)
+					min = current->data;
+				else
+				{
+					sortMin = 1;
+					break;
+				}
+			}
+			current = current->next;
+		} while (current != NULL);
+		if (sortMin == 0 || sortMax == 0)
+		{
+			std::cout << "List is sorted" << std::endl;
+			return true;
+		}
+		else
+		{
+			std::cout << "List is NOT sorted" << std::endl;
+			return false;
+		}
+	}
+}
+
+int main()
+{
+	List* list = new List;
+	init(list);
+	char bracket = ' ';
+	std::cout << "Enter a bracket sequence. For exit enter 0" << std::endl;
+	while (bracket != '0')
+	{
+		std::cin >> bracket;
+		if (bracket != '0') ins(list, bracket);
+	}
+	checkBracket(list);
+	deleteList(list);
+	delete list;
+
+	List* lst = new List;
+	init(lst);
+	for (size_t i = 0; i < 10; i++)
+	{
+		ins(lst, rand() % 100);
+	}
+	printList(lst);
+
+	List* copyLst = new List;
+	copyLst = copyList(lst);
+
+	deleteList(lst);
+	delete lst;
+
+	std::cout << "Copy of list" << std::endl;
+	printList(copyLst);
+
+	deleteList(copyLst);
+	delete copyLst;
+
+	List* lstSort = new List;
+	init(lstSort);
+	int n;
+	std::cout << "Enter 10 numbers" << std::endl;
+	for (size_t i = 0; i < 10; i++)
+	{
+		std::cin >> n;
+		ins(lstSort, n);
+	}
+	printList(lstSort);
+
+	checkSort(lstSort);
+
+	deleteList(lstSort);
+	delete lstSort;
 }
